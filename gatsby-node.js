59e3cf9e08dd4c98e26fc246d01a2b4629057777
JThorneX <115962097+JThorneX@users.noreply.github.com>
2023-07-29 23:16:13 -0600

    fixed conflicting files

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,7 +4,6 @@ const _ = require("lodash");
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const { createPage } = actions;
 
-  const blogPostTemplate = path.resolve("src/templates/blog.js");
   const tagTemplate = path.resolve("src/templates/tags.js");
 
   const result = await graphql(`
@@ -32,18 +31,6 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
     reporter.panicOnBuild(`Error while running GraphQL query.`);
     return;
   }
-
-  const posts = result.data.postsMdx.edges;
-
-  // Create post detail pages
-  posts.forEach(({ node }) => {
-    createPage({
-      path: node.frontmatter.slug,
-      component: blogPostTemplate,
-    });
-  });
-
-  // Extract tag data from query
   const tags = result.data.tagsGroup.group;
 
   // Make tag pages
